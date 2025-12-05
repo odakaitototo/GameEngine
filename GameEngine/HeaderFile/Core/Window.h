66701e2d53d19092dcd2f473f23be43303fd4df0
かいと <kuidouzhitian084@gmail.com>
2025-12-05 16:40:30 +0900

    試運転 ファイルの作成

---
+++ GameEngine/HeaderFile/Core/Window.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Windows.h>
+
+class Window
+{
+public:
+	Window();
+	~Window();
+
+	// ウィンドウを作成し、表示する
+	// width、height:解像度、title: ウィンドウ名
+	bool Init(HINSTANCE hInstance, int width, int height, const char* title);
+
+	// メッセージを処理する（ゲーム内で毎フレーム呼ぶ）
+	// 戻り値:　trueなら継続、falseなら終了
+	bool ProcessMessage();
+
+	// ウィンドウハンドルを取得（DirectX初期化で必要）
+	HWND GetHandle() const { return m_hwnd; }
+
+private:
+	HWND m_hwnd; // ウィンドウハンドル
+	WNDCLASSEX m_wc; // ウィンドウクラス情報
+};
